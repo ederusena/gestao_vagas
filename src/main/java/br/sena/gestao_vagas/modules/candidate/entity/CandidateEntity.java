5414f52d7ca8281dbcc4e38d4bc5a276fcad3d83
Eder Sena <edersena.ehs@motiva.com.br>
2025-11-11 14:16:48 -0300

    Adiciona a entidade CandidateEntity com validações e campos necessários; atualiza configurações do application.properties

--- src/main/java/br/sena/gestao_vagas/modules/candidate/entity/CandidateEntity.java
+++ src/main/java/br/sena/gestao_vagas/modules/candidate/entity/CandidateEntity.java
@@ -1,17 +1,27 @@
 package br.sena.gestao_vagas.modules.candidate.entity;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+  
   private String name;
 
   @Email(message = "Invalid email format")
@@ -26,5 +36,7 @@ public class CandidateEntity {
   private String description;
   private String resume;
   
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 }
